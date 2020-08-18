21476e84b0f89fb8dd2b039686c1e60d30043699
sonia23cld <sonia.celestini@gmi.oeaw.ac.at>
2020-08-18 17:15:53 +0200

    Transcription Factor Enrichment for modules of interest. Added correction for multiple testing and gene name extraction for significant TF

--- Scripts/Transcripion_Factor_Enrichment.R
+++ Scripts/Transcripion_Factor_Enrichment.R
@@ -41,8 +41,8 @@ AraEnrice_v1 <- function(genes, OG, bg_genes = NULL, n_res = 25) {
   #############################################################################
   # Plot the results!
   sort_res <- sort(res, index.return = TRUE, decreasing = FALSE)
-  output <- data.frame(matrix(NA, nrow = n_res, ncol = 7)) 
-  colnames(output)<- c('name', 'log10_pval', 'group_index', 'groups_size', 'sample_size', 'intersect_size', 'set')    
+  output <- data.frame(matrix(NA, nrow = n_res, ncol = 8)) 
+  colnames(output)<- c('name', 'log10_pval', 'padj', 'group_index', 'groups_size', 'sample_size', 'intersect_size', 'set')    
   writeLines("using log10") 
   for(i in 1:n_res) {
     g_i <- sort_res$ix[i]
@@ -62,6 +62,7 @@ AraEnrice_v1 <- function(genes, OG, bg_genes = NULL, n_res = 25) {
       output$set[i],':',output$name[i],'\r\n',
       sep=""))
   }
+  output$padj<- p.adjust(10^output$log10_pval, method = 'BH', n=349)
   invisible(output)
 }
 
@@ -90,7 +91,8 @@ OG$g_info <- OG$g_info[ind,]
 OG$groups <- OG$groups[,ind]
 
 #TF Enrichment
-Enrichment<- AraEnrice_v1(genes, OG, n_res = 50)
+Enrichment<- AraEnrice_v1(genes, OG, n_res = 349)
+Enrichment<- Enrichment[Enrichment$padj < 0.05,]
 write.table(Enrichment, file = '/Volumes/nordborg/pub/forPieter/WGCNA/WGCNA_8acn/TF_enrichment_module31.txt', sep = '\t', quote = F, row.names = F)
 
 #repeat only for GxT genes in module 31
@@ -103,13 +105,14 @@ for(i in 1:length(gxt_31)) {
   genes_gxt <- c(genes_gxt, which(gene_infoV2$Name == gxt_31[i]))
 }
 
-Enrichment_gxt<- AraEnrice_v1(genes_gxt, OG, n_res = 50)
+Enrichment_gxt<- AraEnrice_v1(genes_gxt, OG, n_res = 349)
+Enrichment_gxt<- Enrichment_gxt[Enrichment_gxt$padj < 0.05,]
 write.table(Enrichment_gxt, file = '/Volumes/nordborg/pub/forPieter/WGCNA/WGCNA_8acn/TF_enrichment_module31_onlyGxTgenes.txt', sep = '\t', quote = F, row.names = F)
 
-
+###########################################
 #check which genes have a specific TF motif
 #check for CBF family
-check_Tf<-OG$groups[genes,Enrichment$group_index[c(1,7, 11, 12)]] #here you mean whether your genes are in group 23 ecc...? I.e. the syntax is OG$groups[genei, groupj]?
+check_Tf<-OG$groups[genes, Enrichment$group_index[c(1,7, 11, 12)]] #here you mean whether your genes are in group 23 ecc...? I.e. the syntax is OG$groups[genei, groupj]?
 #If they are present you will have a 1 
 
 CBF_genes<-which(apply(check_Tf, 1, function(x) {any(x %in% 1)}))
@@ -117,10 +120,39 @@ CBF_genes<- genes[CBF_genes]
 CBF_genes<-  gene_infoV2$Name[CBF_genes]
 
 #check for DDF family
-check_Tf<-OG$groups[genes,Enrichment$group_index[c(3, 6)]] 
+check_Tf<-OG$groups[genes, Enrichment$group_index[c(3, 6)]] 
 DDF_genes<-which(apply(check_Tf, 1, function(x) {any(x %in% 1)}))
 DDF_genes<- genes[DDF_genes]
 DDF_genes<-  gene_infoV2$Name[DDF_genes]
+
+###########################################
+
+#you will have to do SNPs analysis on significant TF genes you spotted. You will get the gene name coding for the TF in the gff file.
+TF_families<- unique(c(Enrichment$name, Enrichment_gxt$name))
+
+#impot and clean gff file
+Araport11 <- read.gff('/Volumes/nordborg/user/sonia.celestini/Araport11_GFF3_genes_transposons.201606.gff') 
+Araport11[,9:10]<- str_split_fixed(Araport11$attributes, ';', 2)
+Araport11<- Araport11[Araport11$type %in% 'gene',]
+Araport11$attributes<- str_split_fixed(Araport11$attributes, 'ID=', 2)[,2]
+colnames(Araport11)[9]<- 'ID'
+colnames(Araport11)[10]<- 'attributes'
+
+
+#clean TF names
+TF_families<- str_split_fixed(TF_families, '_', 2)[,1]
+
+#get the genes name
+TF_families<- gsub("t", "T", TF_families)
+TF_families<- gsub("g", "G", TF_families)
+TF_genes<- list()
+for (i in TF_families) {
+  TF_genes[[i]]<- Araport11$ID[grep(i, Araport11$attributes)]
+}
+
+save(TF_genes, file = '/Volumes/nordborg/user/sonia.celestini/WGCNA_newData/TFgenes_module31.RData')
+
+
 
 
 
